e8dce9b96d31e01a3dda55f66ed2d530af7daaa6
Saans <92422813+saans808@users.noreply.github.com>
2022-03-09 22:09:57 +0900

    Mogakko day3

---
+++ day3.js
@@ -0,0 +1,14 @@
+function changeText0(){
+    var h1 = document.getElementsByTagName('h1');
+    h1[0].innerHTML = "모진 시간을 견디며"
+}
+
+function changeText1(){
+    var h1 = document.getElementsByTagName('h1');
+    h1[1].innerHTML = "각오를 단단히 다지고 공부해"
+}
+
+function changeText2(){
+    var h1 = document.getElementsByTagName('h1');
+    h1[2].innerHTML = "코딩마스터가 되자"
+}
