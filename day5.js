518d7c39645ab3b0fb0a117fc3dcd15cf57f2bab
Saans <92422813+saans808@users.noreply.github.com>
2022-03-14 15:17:25 +0900

    mogakko day5

---
+++ day5.js
@@ -0,0 +1,18 @@
+var red = "color of apple";
+var swift = {name: "taylor", birth: 1984};
+var cathy = {age: 20, pet: "cat"};
+
+num = 10;
+int = "like";
+str = "문자열"; 
+
+console.log(typeof(3));
+console.log(typeof(red));
+console.log(typeof(true));
+console.log(typeof(swift));
+console.log(typeof(cathy.age));
+console.log(typeof(str));
+console.log(typeof(int));
+console.log(typeof(num));
+console.log(typeof(null));
+console.log(typeof(undefined));
